312131739f1214208563c4fa612b3dac1dd85b07
huybinh0204 <huybinh020403@gmail.com>
2021-01-12 23:58:45 +0700

    update post s

--- service.js
+++ service.js
@@ -1,17 +1,17 @@
 const mysql = require('mysql');
 
 const connection = mysql.createConnection({
-    // host: "192.168.64.2",
-    // user: "user",
-    // password: "Abc123",
-    // database: "vietnails_vip",
-    // port: 3306,
-    //-------
-    host: "localhost",
-    user: "root",
-    password: "bYl@12345^^",
-    database: "ntk_vietnails",
+    host: "192.168.64.2",
+    user: "user",
+    password: "Abc123",
+    database: "vietnails_vip",
     port: 3306,
+    //-------
+    // host: "localhost",
+    // user: "root",
+    // password: "bYl@12345^^",
+    // database: "ntk_vietnails",
+    // port: 3306,
 });
 connection.connect(function(err) {
     if (err) throw err;
